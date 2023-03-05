050cc30857290e46be54af165d01f4bfb0ebfa88
dimySenpai <dimitrymabom@gmail.com>
2023-03-05 22:49:27 +0100

    cra-config-perso-with-bulma

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,8 @@ import { render } from 'react-dom';
 import 'bulma/css/bulma.min.css';
 
 render(
-  <>
-  
-  </>,
+  <div className='title is-1 is-primary'>
+  hello world !!!
+  </div>,
   document.getElementById('app')
 );
